abc98c382df6ab2c247a030f0572925371cc27c1
joiellantero <jtllantero@up.edu.ph>
2021-04-25 15:13:15 +0800

    run three tests

--- pinsetter.py
+++ pinsetter.py
@@ -26,31 +26,51 @@ if __name__ == "__main__":
     d = {
         'Algorithm': ['Formula', 'Recursion', 'One Loop'],
         'Result': ['-', '-', '-'],
-        'Speed': ['-', '-', '-']
+        'Speed_Test1': ['-', '-', '-'],
+        'Speed_Test2': ['-', '-', '-'],
+        'Speed_Test3': ['-', '-', '-']
     }
 
     df = pd.DataFrame(data=d)
 
-    n = int(input("input: "))
+    n = int(input("Input: "))
 
-    start_formula = timeit.default_timer()
-    res_formula = pinsetter_formula(n)
-    stop_formula = timeit.default_timer()
+    res_formula_list = []
+    res_rec_list = []
+    res_loop_list = []
+    
+    for i in range(0, 3):
+        start_formula = timeit.default_timer()
+        res_formula = pinsetter_formula(n)
+        stop_formula = timeit.default_timer()
+        res_formula_list.append("{:e}".format(stop_formula-start_formula))
 
-    start_loop = timeit.default_timer()
-    res_loop = pinsetter_loop(n)
-    stop_loop = timeit.default_timer()
+    for i in range(0, 3):
+        start_recursion = timeit.default_timer()
+        res_rec = pinsetter_recursion(n)
+        stop_recursion = timeit.default_timer()
+        res_rec_list.append("{:e}".format(stop_recursion-start_recursion))
 
-    start_recursion = timeit.default_timer()
-    res_rec = pinsetter_recursion(n)
-    stop_recursion = timeit.default_timer()
+    for i in range(0, 3):
+        start_loop = timeit.default_timer()
+        res_loop = pinsetter_loop(n)
+        stop_loop = timeit.default_timer()
+        res_loop_list.append("{:e}".format(stop_loop-start_loop))
 
     df.loc[0].Result = int(res_formula)
     df.loc[1].Result = res_rec
     df.loc[2].Result = res_loop
 
-    df.loc[0].Speed = "{:e}".format(stop_formula-start_formula)
-    df.loc[1].Speed = "{:e}".format(stop_recursion-start_recursion)
-    df.loc[2].Speed = "{:e}".format(stop_loop-start_loop)
+    df.loc[0].Speed_Test1 = res_formula_list[0]
+    df.loc[1].Speed_Test1 = res_rec_list[0]
+    df.loc[2].Speed_Test1 = res_loop_list[0]
 
-    print(df)
+    df.loc[0].Speed_Test2 = res_formula_list[1]
+    df.loc[1].Speed_Test2 = res_rec_list[1]
+    df.loc[2].Speed_Test2 = res_loop_list[1]
+
+    df.loc[0].Speed_Test3 = res_formula_list[2]
+    df.loc[1].Speed_Test3 = res_rec_list[2]
+    df.loc[2].Speed_Test3 = res_loop_list[2]
+
+    print('\n', df, '\n')
